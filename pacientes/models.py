18ffc7809723a002e039f54c7aff4650a8e0687b
gustavocarvalho1997 <gustavocarvalho@hotmail.com>
2025-02-15 21:43:53 -0300

    add model Paciente

--- pacientes/models.py
+++ pacientes/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Paciente(models.Model):
+    queixa_choices = (
+        ('TDAH', 'TDAH'),
+        ('D', 'Depressão'),
+        ('A', 'Ansiedade'),
+        ('O', 'Obsessão'),
+        ('P', 'Pânico'),
+        ('TAG', 'Transtorno de Ansiedade Generalizada'),
+    )
+
+    nome = models.CharField(max_length=255)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=255, null=True, blank=True)
+    foto = models.ImageField(upload_to='fotos')
+    pagamento_em_dia = models.BooleanField(default=True)
+    queixa = models.CharField(max_length=4, choices=queixa_choices)
+
+    def __str__(self):
+        return self.nome
